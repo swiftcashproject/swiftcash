0e60ecfc50a47db49f5d53433dcd7a862b932765
bitcoiner <thebitcoiner@users.noreply.github.com>
2018-11-05 03:07:28 -0800

    Update rpcswiftnode.cpp

--- src/rpcswiftnode.cpp
+++ src/rpcswiftnode.cpp
@@ -261,6 +261,7 @@ UniValue listswiftnodes(const UniValue& params, bool fHelp)
             obj.push_back(Pair("outidx", (uint64_t)oIdx));
             obj.push_back(Pair("status", strStatus));
             obj.push_back(Pair("addr", CBitcoinAddress(mn->pubKeyCollateralAddress.GetID()).ToString()));
+            obj.push_back(Pair("netaddr", mn->addr.ToString()));
             obj.push_back(Pair("version", mn->protocolVersion));
             obj.push_back(Pair("lastseen", (int64_t)mn->lastPing.sigTime));
             obj.push_back(Pair("activetime", (int64_t)(mn->lastPing.sigTime - mn->sigTime)));
