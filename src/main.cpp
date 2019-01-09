082209620b2c6f958044e534ef19bb97e68df637
msg768 <markus.ghambari@gmail.com>
2019-01-09 16:13:38 +1100

    changed DarkNet to SwiftCash

--- src/main.cpp
+++ src/main.cpp
@@ -96,7 +96,7 @@ static void CheckBlockIndex();
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "DarkNet Signed Message:\n";
+const string strMessageMagic = "SwiftCash Signed Message:\n";
 
 // Internal stuff
 namespace
