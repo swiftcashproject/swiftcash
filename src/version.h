4bd174cb8b0f6e88f317122ef96b3c949ccc7f24
msg768 <markus.ghambari@gmail.com>
2019-01-09 16:11:10 +1100

    version1.2

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2012-2014 Bitcoin developers
 // Copyright (c) 2014-2015 Dash developers
 // Copyright (c) 2015-2018 PIVX developers
-// Copyright (c) 2018 SwiftCash developers
+// Copyright (c) 2018-2019 SwiftCash developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -12,14 +12,14 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 80412;
+static const int PROTOCOL_VERSION = 80413;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 200;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 80412;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80413;
+static const int MIN_PEER_PROTO_VERSION = 80413;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80414;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
