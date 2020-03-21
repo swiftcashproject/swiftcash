0b5cd4aa2d2e80add3118073e1515f9be13b3e2c
msg768 <markus.ghambari@gmail.com>
2020-03-21 16:02:27 +1100

    v3.0 default mining params

--- src/main.h
+++ src/main.h
@@ -58,14 +58,14 @@ struct CNodeStateStats;
 /** Default variable for nBudgetPaid passed by reference **/
 static CAmount ZERO_AMOUNT = 0;
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 1048576; // 1MB
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 524288; // 500KB
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
-static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
+static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 51200; // 50KB
 /** Default for accepting alerts from the P2P network. */
 static const bool DEFAULT_ALERTS = true;
 /** The maximum size for transactions we're willing to relay/mine */
-static const unsigned int MAX_STANDARD_TX_SIZE = 100000;
+static const unsigned int MAX_STANDARD_TX_SIZE = 102400; // 100 KB
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE / 50;
 /** Maximum number of signature check operations in an IsStandard() P2SH script */
