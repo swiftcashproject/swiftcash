6f43063e5877e38b12b74f6187a3faf97406ddf8
Gregory Lemercier <greglemercier@free.fr>
2021-02-20 23:13:01 +0100

    Add 'sendtoaddresslocked' RPC call to send time-locked transactions

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -406,6 +406,7 @@ static const CRPCCommand vRPCCommands[] =
         {"wallet", "lottery", &lottery, false, false, true},
         {"wallet", "hodldeposit", &hodldeposit, false, false, true},
         {"wallet", "sendtoaddress", &sendtoaddress, false, false, true},
+        {"wallet", "sendtoaddresslocked", &sendtoaddresslocked, false, false, true},
         {"wallet", "sendtoaddressix", &sendtoaddressix, false, false, true},
         {"wallet", "setaccount", &setaccount, true, false, true},
         {"wallet", "setstakesplitthreshold", &setstakesplitthreshold, false, false, true},
